a014ca1c44ad38805bb4938c951e0110e9a8701c
serinryu <serin0404@naver.com>
2023-09-07 13:50:49 +0900

    Init

---
+++ src/main/java/gdsc/be/mount/MountApplication.java
@@ -0,0 +1,13 @@
+package gdsc.be.mount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gdsc/be/mount/MountApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.be.mount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
