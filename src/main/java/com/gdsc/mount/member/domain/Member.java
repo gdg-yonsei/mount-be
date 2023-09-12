77a7d37e2d9f994c295ea072e5d2bb4385f56712
becooq81 <becooq81@gmail.com>
2023-09-12 21:12:29 +0900

    feat: member entity

--- src/main/java/com/gdsc/mount/member/domain/Member.java
+++ src/main/java/com/gdsc/mount/member/domain/Member.java
@@ -1,2 +1,28 @@
-package com.gdsc.mount.member.domain;public class Member {
+package com.gdsc.mount.member.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "members")
+@Getter
+public class Member {
+
+    @Id
+    private String _id;
+
+    @Field(name = "username")
+    @Indexed(unique = true)
+    private String username;
+
+    protected Member() {}
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void updateUsername(String username) {
+        this.username = username;
+    }
 }
