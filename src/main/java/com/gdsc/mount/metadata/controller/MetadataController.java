3c66bbdc058b924f41b8c6a24b104659e4a2b32e
becooq81 <becooq81@gmail.com>
2023-09-12 21:14:05 +0900

    feat: file controller

--- src/main/java/com/gdsc/mount/metadata/controller/MetadataController.java
+++ src/main/java/com/gdsc/mount/metadata/controller/MetadataController.java
@@ -1,2 +1,70 @@
-package com.gdsc.mount.metadata.controller;public class MetadataController {
+package com.gdsc.mount.metadata.controller;
+
+
+import com.gdsc.mount.metadata.domain.Metadata;
+import com.gdsc.mount.metadata.dto.MetadataResponse;
+import com.gdsc.mount.metadata.service.MetadataService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/file")
+@RequiredArgsConstructor
+public class MetadataController {
+    private final MetadataService metadataService;
+
+    // get file
+    @GetMapping("/{fileId}")
+    public ResponseEntity<MetadataResponse> findFileById(@PathVariable String fileId) {
+        Metadata metadata = metadataService.getMetadatabyId(fileId);
+        return ResponseEntity.status(200).body(MetadataResponse.of(metadata));
+    }
+
+    // get all
+    @GetMapping("/all")
+    public ResponseEntity<List<MetadataResponse>> findFilesByPage(@RequestParam int page, @RequestParam int size) {
+        return ResponseEntity.status(200).body(metadataService.getAllByPage(page, size));
+    }
+
+    // upload file
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
+                                             @RequestParam("username") String username)
+            throws Exception {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        String fileCode = metadataService.uploadFile(fileName, file, username);
+        return ResponseEntity.status(201).body(fileCode);
+    }
+
+    // download file
+    @GetMapping("/download/{fileId}")
+    public ResponseEntity<?> downloadFile(@RequestParam("username") String username, @PathVariable String fileId) {
+        Resource resource;
+        try {
+            resource = metadataService.downloadFile(username, fileId);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+        if (resource == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found.");
+        }
+
+        return ResponseEntity.ok().body(resource);
+    }
+
+    // delete file
+    @DeleteMapping("/{fileId}")
+    public ResponseEntity<String> deleteFile(@RequestParam("username") String username, @PathVariable String fileId) throws Exception{
+        boolean success = metadataService.deleteFile(username, fileId);
+        String body = success ? "success" : "fail";
+        return ResponseEntity.status(204).body(body);
+    }
 }
