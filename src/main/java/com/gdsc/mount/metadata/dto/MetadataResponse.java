f048f778f21d552dcd9feb6c9888a812e2c7f7e7
becooq81 <becooq81@gmail.com>
2023-09-12 21:14:41 +0900

    feat: metadata response

--- src/main/java/com/gdsc/mount/metadata/dto/MetadataResponse.java
+++ src/main/java/com/gdsc/mount/metadata/dto/MetadataResponse.java
@@ -1,2 +1,42 @@
-package com.gdsc.mount.metadata.dto;public class MetadataResponse {
+package com.gdsc.mount.metadata.dto;
+
+import com.gdsc.mount.metadata.domain.Metadata;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class MetadataResponse {
+    private String _id;
+    private String fileName;
+    private String username;
+    private String contentType;
+    private Long sizeInBytes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    protected MetadataResponse() {}
+    public MetadataResponse(String _id, String fileName, String username, String contentType, Long sizeInBytes, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this._id = _id;
+        this.fileName = fileName;
+        this.username = username;
+        this.contentType = contentType;
+        this.sizeInBytes = sizeInBytes;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static MetadataResponse of(Metadata metadata) {
+        return MetadataResponse.builder()
+                ._id(metadata.get_id())
+                .fileName(metadata.getFileName())
+                .username(metadata.getUsername())
+                .contentType(metadata.getContentType())
+                .sizeInBytes(metadata.getSizeInBytes())
+                .createdAt(metadata.getCreatedAt())
+                .updatedAt(metadata.getUpdatedAt())
+                .build();
+    }
 }
