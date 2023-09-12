5e3e2c80782f9ebd951799d3d527a24230ebaf44
becooq81 <becooq81@gmail.com>
2023-09-12 21:14:55 +0900

    feat: metadata response

--- src/main/java/com/gdsc/mount/metadata/repository/MetadataRepository.java
+++ src/main/java/com/gdsc/mount/metadata/repository/MetadataRepository.java
@@ -1,2 +1,10 @@
-package com.gdsc.mount.metadata.repository;public interface MetadataRepository {
+package com.gdsc.mount.metadata.repository;
+
+import com.gdsc.mount.metadata.domain.Metadata;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MetadataRepository extends MongoRepository<Metadata, String> {
 }
