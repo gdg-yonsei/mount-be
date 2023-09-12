55fb2788542edad8497ce017a52b108b98d7fbe9
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-09-12 23:10:46 +0900

    style

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
-import models
-from database import engine
-from routers import file, folder
+from file.controller import filecontroller
+import file.service.filemodels as filemodels
+from file.service.database import engine
+
 import uvicorn
+import os
+
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
 
-app.include_router(file.router)
-app.include_router(folder.router)
+filemodels.Base.metadata.create_all(bind=engine)
+
+app.include_router(filecontroller.router)
+#app.include_router(foldercontroller.router)
 
 if __name__ == '__main__':
     uvicorn.run(app,host = '0.0.0.0',port = 8000)
