f6a576cf18dedfde2b3d470394c58f42934c3400
becooq81 <becooq81@gmail.com>
2023-09-12 20:41:58 +0900

    Initial commit

---
+++ src/main/java/com/gdsc/mount/MountApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.mount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountApplication.class, args);
+	}
+
+}
