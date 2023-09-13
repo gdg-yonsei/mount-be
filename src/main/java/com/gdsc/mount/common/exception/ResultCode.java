20af2616b7f6e46f341615ccd85ffc5af3df9c80
becooq81 <becooq81@gmail.com>
2023-09-14 01:57:12 +0900

    feat: result code enum to unify exception handling

--- src/main/java/com/gdsc/mount/common/exception/ResultCode.java
+++ src/main/java/com/gdsc/mount/common/exception/ResultCode.java
@@ -1,23 +1,33 @@
-package com.gdsc.mount.exception;
+package com.gdsc.mount.common.exception;
 
-public enum NotFoundResultCode {
-    MEMBER_NOT_FOUND(600, "MEMBER_NOT_FOUND"),
-    METADATA_NOT_FOUND(601, "METADATA_NOT_FOUND");
+import lombok.Getter;
+
+@Getter
+public enum ResultCode {
+    SUCCESS(200, ResultMessage.SUCCESS),
+    SUCCESS_CREATED(201, ResultMessage.SUCCESS_CREATED),
+    INTERNAL_SERVER_ERROR(500, ResultMessage.INTERNAL_SERVER_ERROR),
+    MEMBER_NOT_FOUND(600, ResultMessage.MEMBER_NOT_FOUND),
+    METADATA_NOT_FOUND(601, ResultMessage.METADATA_NOT_FOUND),
+    EXISTING_USERNAME(602, ResultMessage.EXISTING_USERNAME),
+    UNAUTHORIZED_DOWNLOAD_ATTEMPT(603, ResultMessage.UNAUTHORIZED_DOWNLOAD_ATTEMPT);
 
     private final int code;
     private final String message;
 
-    private NotFoundResultCode(int code, String message) {
+    ResultCode(int code, String message) {
         this.code = code;
         this.message = message;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
+    public interface ResultMessage {
+        String SUCCESS = "Successfully processed.";
+        String MEMBER_NOT_FOUND = "No member exists with given id.";
+        String METADATA_NOT_FOUND = "No metadata exists with given id.";
+        String EXISTING_USERNAME = "Member with given username already exists.";
+        String INTERNAL_SERVER_ERROR = "Internal server error.";
+        String UNAUTHORIZED_DOWNLOAD_ATTEMPT = "You can only download the file you uploaded.";
+        String SUCCESS_CREATED = "Successfully created.";
     }
 
 }
