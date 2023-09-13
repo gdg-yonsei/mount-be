3a50d8a3eac0599435ee95669fd20076cf2fe155
becooq81 <becooq81@gmail.com>
2023-09-14 01:56:16 +0900

    fix: local date time -> instant

--- src/main/java/com/gdsc/mount/metadata/dto/MetadataResponse.java
+++ src/main/java/com/gdsc/mount/metadata/dto/MetadataResponse.java
@@ -1,24 +1,25 @@
 package com.gdsc.mount.metadata.dto;
 
 import com.gdsc.mount.metadata.domain.Metadata;
+import com.gdsc.mount.common.dto.BaseResponse;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Getter
 @Builder
-public class MetadataResponse {
+public class MetadataResponse extends BaseResponse {
     private String _id;
     private String fileName;
     private String username;
     private String contentType;
     private Long sizeInBytes;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    private Instant createdAt;
+    private Instant updatedAt;
 
     protected MetadataResponse() {}
-    public MetadataResponse(String _id, String fileName, String username, String contentType, Long sizeInBytes, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public MetadataResponse(String _id, String fileName, String username, String contentType, Long sizeInBytes, Instant createdAt, Instant updatedAt) {
         this._id = _id;
         this.fileName = fileName;
         this.username = username;
