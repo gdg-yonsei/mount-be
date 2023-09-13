1f7a3c64e52c2651d550802e856b253e30ab098d
becooq81 <becooq81@gmail.com>
2023-09-14 03:28:50 +0900

    feat: added error handler

--- src/main/java/com/gdsc/mount/member/controller/MemberController.java
+++ src/main/java/com/gdsc/mount/member/controller/MemberController.java
@@ -1,18 +1,27 @@
 package com.gdsc.mount.member.controller;
 
+import com.gdsc.mount.common.exception.ErrorResponse;
 import com.gdsc.mount.member.dto.CreateMemberRequest;
 import com.gdsc.mount.member.dto.MemberResponse;
 import com.gdsc.mount.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+
+import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+
 
     // get member
     @GetMapping("/{memberId}")
@@ -22,9 +31,9 @@ public class MemberController {
 
     // create member
     @PostMapping("/new")
-    public ResponseEntity<String> registerMember(@RequestBody CreateMemberRequest request) {
-        String id = memberService.createMember(request.getUsername());
-        return ResponseEntity.status(201).body(id);
+    public ResponseEntity<MemberResponse> registerMember(@RequestBody CreateMemberRequest request) {
+        MemberResponse response = memberService.createMember(request.getUsername());
+        return ResponseEntity.status(201).body(response);
     }
 
     // delete member
@@ -33,4 +42,50 @@ public class MemberController {
         memberService.deleteMemberById(memberId);
         return ResponseEntity.status(204).body("Successfully deleted.");
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementFoundException(
+            NoSuchElementException exception, WebRequest request
+    ) {
+        ErrorResponse response = ErrorResponse.of(
+                exception,
+                "No such element exists.",
+                HttpStatus.NOT_FOUND,
+                request
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            WebRequest request
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Validation error. Check 'errors' field for details."
+        );
+
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errorResponse.addValidationError(fieldError.getField(),
+                    fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.unprocessableEntity().body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ErrorResponse> handleAllUncaughtException(
+            Exception exception,
+            WebRequest request){
+        ErrorResponse response = ErrorResponse.of(exception,
+                "An unexpected error occurred.",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+
 }
