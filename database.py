405b3440597af802ad620133a1f432b030252e06
Chaerim <cofla2020@yonsei.ac.kr>
2023-09-15 00:35:02 +0900

    feat: add .env, config.py for environment variable management

--- database.py
+++ database.py
@@ -4,11 +4,12 @@ database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import Config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mydatabase.db"
+config = Config()
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    config.DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ main.py
 from fastapi import FastAPI
 from file.controller import fileController
 from database import engine, Base
+from config import Config
 import uvicorn
 
 app = FastAPI()
+config = Config()
 
 Base.metadata.create_all(bind=engine)
 app.include_router(fileController)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("main:app", host=config.HOST, port=config.PORT, reload=True)
