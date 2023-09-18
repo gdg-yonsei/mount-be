8b0f36acf44ed1f3b85be6be5d66808dac4d1faf
becooq81 <becooq81@gmail.com>
2023-09-19 00:57:21 +0900

    feat: directory service

--- src/main/java/com/gdsc/mount/directory/service/DirectoryService.java
+++ src/main/java/com/gdsc/mount/directory/service/DirectoryService.java
@@ -1,2 +1,32 @@
-package com.gdsc.mount.directory.service;public class DirectoryService {
+package com.gdsc.mount.directory.service;
+
+import com.gdsc.mount.directory.domain.Directory;
+import com.gdsc.mount.directory.dto.DirectoryCreateRequest;
+import com.gdsc.mount.directory.repository.DirectoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class DirectoryService {
+    private final DirectoryRepository directoryRepository;
+
+    public void createDirectory(DirectoryCreateRequest request) {
+        Directory directory;
+        if (request.getParentDirectoryId() != null) {
+            Directory parentDirectory = findParentDirectory(request.getParentDirectoryId());
+            directory = new Directory(request.getName(), parentDirectory, parentDirectory.getPath() + "/" + request.getName(), false);
+            parentDirectory.addDirectory(directory);
+        } else {
+            directory = new Directory(request.getName(), null, "/" + request.getName(), true);
+        }
+        directoryRepository.save(directory);
+    }
+
+    public Directory findParentDirectory(String parentDirectoryId) {
+        return directoryRepository.findById(parentDirectoryId)
+                .orElseThrow(() -> new NoSuchElementException("No directory found with id: " + parentDirectoryId));
+    }
 }
