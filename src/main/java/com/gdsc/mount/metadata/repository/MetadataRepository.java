48ab1ec3c3d52d4ed91571b1a186c3521d8fd00d
becooq81 <becooq81@gmail.com>
2023-09-19 02:08:28 +0900

    feat(MetadataRepository): find all by name

--- src/main/java/com/gdsc/mount/metadata/repository/MetadataRepository.java
+++ src/main/java/com/gdsc/mount/metadata/repository/MetadataRepository.java
@@ -4,7 +4,10 @@ import com.gdsc.mount.metadata.domain.Metadata;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface MetadataRepository extends MongoRepository<Metadata, String> {
+    List<Metadata> findAllByName(String name);
 }
