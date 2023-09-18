f86d2c2428086cb538a7da4564fbe9fca75d11fa
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-09-18 14:30:53 +0900

    feat : update folder logic

--- folder/controller.py
+++ folder/controller.py
@@ -1,67 +1,35 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from datetime import datetime
 
 from fastapi import APIRouter, Depends, HTTPException
 from model.models import Folders
-from utils.utils import get_db
+from utils.utils import get_db 
 from folder.service import (
-    update_children_folder,
-    save_folder,
     get_folder,
+    create_new_folder,
+    update_folder_info
 )
 
 folderController = APIRouter(prefix="/folder")
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
 """ 
 POST : Create folder for specific user
 """
 @folderController.post("/{username}/create")
 async def create_folder(
-    db: db_dependency,
-    folder_name: str,
-    username: str,
-    parent_name: str = "root",
-):
+        db: db_dependency,
+        folder_name: str,
+        username: str,
+        parent_name: str = "root",
+    ):
     existing_folder = get_folder(db, username, folder_name)
     if existing_folder:
         raise HTTPException(status_code=400, detail="Folders already exist")
     
-    # 시작 root 폴더 생성
-    if parent_name == 'start':
-        new_folder = Folders(
-        original_name= 'root',
-        stored_name= 'root',
-        file_size=0,
-        uploader=username,
-        uploaded_time=current_time,
-        modified_time=current_time,
-        is_folder=True,
-        parent_id= 0
-        )
-    
-    else:
-        parent_folder = get_folder(db, username, parent_name)
-        
-        new_folder = Folders(
-        original_name=folder_name,
-        stored_name=folder_name,
-        file_size=0,
-        uploader=username,
-        uploaded_time=current_time,
-        modified_time=current_time,
-        is_folder=True,
-        parent_id= parent_folder.id
-        )
-        
-        update_children_folder(db, parent_name, username, new_folder)
-        
-
-    save_folder(db, new_folder)
+    new_folder = create_new_folder(db, folder_name, username, parent_name)
 
     return new_folder
 
@@ -95,7 +63,7 @@ async def get_user_folders(db: db_dependency, username: str):
 PUT : Update Folder name and update modified time
 """
 @folderController.put("/{username}/update")
-async def update_folder_name(
+async def update_folder(
     db: db_dependency, username: str, existing_folder_name: str, new_folder_name: str
 ):
     existing_folder = get_folder(db, username, existing_folder_name)
@@ -103,13 +71,8 @@ async def update_folder_name(
     if not existing_folder:
         raise HTTPException(status_code=404, detail="Folder Not Found")
 
-    existing_folder.original_name = new_folder_name
-    existing_folder.stored_name = new_folder_name
-    existing_folder.modified_time = current_time
-
-    db.commit()
+    update_folder_info(db, existing_folder, new_folder_name)
 
     return {
         "message": f"Folder name updated from {existing_folder_name} to {new_folder_name}"
     }
-
--- folder/service.py
+++ folder/service.py
@@ -1,20 +1,57 @@
 import json
+from datetime import datetime
+
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.attributes import flag_modified
 from typing import Annotated
 from fastapi import Depends 
-from model.database import SessionLocal
 from model.models import Folders
+from utils.utils import get_db, get_current_time
+from user.service import check_first_user, add_user
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
         
 db_dependency = Annotated[Session, Depends(get_db)]
+
+current_time = get_current_time()
+
+def create_root_folder(db, username):
+    root_folder = Folders(
+            original_name= 'root',
+            stored_name= 'root',
+            uploader=username,
+            uploaded_time=current_time,
+            modified_time=current_time,
+            parent_id= 0
+        )
+    save_folder(db,root_folder)
+    return root_folder
+
+def create_new_folder(db, folder_name, username, parent_name):
+    
+    user = check_first_user(db, username)
+    
+    if not user:
+        new_folder = create_root_folder(db,username)
+        add_user(db,username)
+        
+    else:
+        parent_folder = get_folder(db, username, parent_name)
+        
+        new_folder = Folders(
+            original_name=folder_name,
+            stored_name=folder_name,
+            uploader=username,
+            uploaded_time=current_time,
+            modified_time=current_time,
+            parent_id= parent_folder.id
+        )
+        
+        update_children_folder(db, parent_name, username, new_folder)
+    
+        save_folder(db, new_folder)
+    return new_folder
+    
+
 
 def get_folder(db, username, folder_name):
     existing_folder = db.query(Folders).filter(
@@ -23,15 +60,24 @@ def get_folder(db, username, folder_name):
         ).first()
     return existing_folder
 
-
-def update_children_file(db: db_dependency, parent_name : str, username :str , uploaded_file):
+def add_child_info(db, child_info, username, parent_name, modified_time):
     
-    parent_folder = db.query(Folders).filter(
-            Folders.original_name == parent_name, 
-            Folders.uploader == username).first()
+    parent_folder = get_folder(db, username, parent_name)
     
     if parent_folder.children is None:
         parent_folder.children = []
+        
+    child_info_json = json.dumps(child_info, ensure_ascii= False)
+    parent_folder.children.append(child_info_json)
+    
+    parent_folder.modified_time = modified_time
+    
+    flag_modified(parent_folder, "children")
+    flag_modified(parent_folder, "modified_time")
+
+def update_children_file(db: db_dependency, parent_name : str, username :str , uploaded_file):
+    
+    modified_time = uploaded_file.uploaded_time
     
     child_info = {
             "name": uploaded_file.original_name,
@@ -40,39 +86,31 @@ def update_children_file(db: db_dependency, parent_name : str, username :str , u
             "created_time": uploaded_file.uploaded_time,
             }
 
-    child_info_json = json.dumps(child_info, ensure_ascii= False)
-    parent_folder.children.append(child_info_json)
+    add_child_info(db, child_info, username,  parent_name , modified_time)
     
-    parent_folder.modified_time = uploaded_file.uploaded_time
-    
-    flag_modified(parent_folder, "children")
-    flag_modified(parent_folder, "modified_time")
     
 def update_children_folder(db: db_dependency, parent_name : int, username :str , new_folder):
-    
-    parent_folder = db.query(Folders).filter(
-            Folders.original_name == parent_name, 
-            Folders.uploader == username).first()
-    
-    if parent_folder.children is None:
-            parent_folder.children = []
+            
+    modified_time = new_folder.uploaded_time
+
     child_info = {
         "name": new_folder.original_name,
         "is_folder": True,  
         "created_time": new_folder.uploaded_time,
         }
     
-    child_info_json = json.dumps(child_info)
-    parent_folder.children.append(child_info_json)
-    parent_folder.modified_time = new_folder.uploaded_time
-    
-    flag_modified(parent_folder, "children")
-    flag_modified(parent_folder, "modified_time")
+    add_child_info(db, child_info, username,  parent_name , modified_time)
+
     
 def save_folder(db, new_folder):
     db.add(new_folder)
     db.commit()
 
 
+def update_folder_info(db, existing_folder, new_folder_name):
+    
+    existing_folder.original_name = new_folder_name
+    existing_folder.stored_name = new_folder_name
+    existing_folder.modified_time = current_time
 
-
+    db.commit()
