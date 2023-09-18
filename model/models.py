4d58ad45b56895ef79519811b421cd2d5e31eca7
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-09-18 14:29:31 +0900

    refactor : Update Table

--- model/models.py
+++ model/models.py
@@ -1,18 +1,16 @@
 from model.database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey , ARRAY
-
+from sqlalchemy import Column, Integer, String, ForeignKey , ARRAY
 
 
 class Folders(Base):
     __tablename__ = 'folders'
+    
     id = Column(Integer, primary_key=True,index=True)
     original_name = Column(String, nullable=False)
     stored_name = Column(String, nullable=True)
-    file_size = Column(Integer, nullable=True)
     uploader = Column(String, nullable=False)
     uploaded_time = Column(String, nullable=False)
     modified_time = Column(String, nullable=True)
-    is_folder = Column(Boolean, default = True)
     parent_id = Column(Integer,ForeignKey('folders.id'), default = 1)
     children = Column(ARRAY(String))
 
@@ -26,7 +24,10 @@ class Files(Base):
     uploader = Column(String, nullable=False)
     uploaded_time = Column(String, nullable=False)
     modified_time = Column(String, nullable=True)
-    is_folder = Column(Boolean, default = False)
     parent_id = Column(Integer,ForeignKey('folders.id'), default = 1)
     
+class Users(Base):
+    __tablename__ = 'users'
     
+    id = Column(Integer, primary_key=True,index=True)
+    username = Column(String, nullable=False)
