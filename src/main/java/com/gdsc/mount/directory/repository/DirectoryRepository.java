816c97b01345459cc20980fe2a62704fbd35f469
becooq81 <becooq81@gmail.com>
2023-09-19 01:35:14 +0900

    feat(DirectoryRepository): find all directories by name value

--- src/main/java/com/gdsc/mount/directory/repository/DirectoryRepository.java
+++ src/main/java/com/gdsc/mount/directory/repository/DirectoryRepository.java
@@ -4,6 +4,10 @@ import com.gdsc.mount.directory.domain.Directory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface DirectoryRepository extends MongoRepository<Directory, String> {
+    List<Directory> findAllByName(String name);
 }
