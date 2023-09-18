a9984b7eb448f5daa657164b6d12bf1308070f0a
becooq81 <becooq81@gmail.com>
2023-09-19 00:35:10 +0900

    feat: directory

--- src/main/java/com/gdsc/mount/directory/domain/Directory.java
+++ src/main/java/com/gdsc/mount/directory/domain/Directory.java
@@ -1,2 +1,26 @@
-package com.gdsc.mount.directory.domain;public class Directory {
+package com.gdsc.mount.directory.domain;
+
+import com.gdsc.mount.common.domain.Node;
+import com.gdsc.mount.common.domain.NodeType;
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Document(collection = "directories")
+public class Directory extends Node {
+
+    @DBRef
+    private List<Node> children = new ArrayList<>();
+
+
+    protected Directory() {}
+
+    public Directory(String name, String parentDirectoryId, String path, boolean atRoot, boolean isLeaf) {
+        super(NodeType.DIRECTORY, name, parentDirectoryId, path, atRoot, isLeaf);
+    }
+
 }
