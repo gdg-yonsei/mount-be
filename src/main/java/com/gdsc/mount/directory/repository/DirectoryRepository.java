9183773724d04600bfe3157ecf01c4880ebc4b48
becooq81 <becooq81@gmail.com>
2023-09-19 00:35:23 +0900

    feat: directory repository

--- src/main/java/com/gdsc/mount/directory/repository/DirectoryRepository.java
+++ src/main/java/com/gdsc/mount/directory/repository/DirectoryRepository.java
@@ -1,2 +1,9 @@
-package com.gdsc.mount.directory.repository;public class DirectoryRepository {
+package com.gdsc.mount.directory.repository;
+
+import com.gdsc.mount.directory.domain.Directory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DirectoryRepository extends MongoRepository<Directory, String> {
 }
