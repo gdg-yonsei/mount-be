a094ea6c63e158e456dbd833e83a1f35cfed70e8
becooq81 <becooq81@gmail.com>
2023-09-19 00:35:01 +0900

    feat: time stamp entity

--- src/main/java/com/gdsc/mount/common/domain/TimestampEntity.java
+++ src/main/java/com/gdsc/mount/common/domain/TimestampEntity.java
@@ -1,2 +1,16 @@
-package com.gdsc.mount.common.domain;public class TimestampEntity {
+package com.gdsc.mount.common.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+
+@Getter
+public class TimestampEntity {
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
 }
